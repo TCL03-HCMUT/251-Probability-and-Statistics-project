9516fcb0d79c46f3dc6126e12cee934b70afb0ee
TCL03 <khoanguyenanh513@gmail.com>
2025-11-16 17:30:44 +0700

    changed analysis flow

--- logistic_regression.r
+++ logistic_regression.r
@@ -6,10 +6,12 @@ library(pROC)
 library(ggplot2)
 
 
+
 # Make this reproducible
 set.seed(1)
 
 data <- read.csv(file = "preprocessed.csv", header = TRUE, sep = ",")
+
 
 split <- sample.split(data, SplitRatio = 0.8)
 
@@ -17,50 +19,51 @@ training <- subset(data, split == "TRUE")
 testing <- subset(data, split == "FALSE")
 
 
-
-# Testing models by adding predictors
-model0 <- glm(Is_Ad ~ 1, training, family = binomial()) # intercept only
-
+# Fitting models
 model1 <- glm(Is_Ad ~ Height + Width, training,
     family = binomial()
-) # only continuous variables
-summary(model1)
-anova(model0, model1)
+) # only Height + Width
 
 model2 <- glm(Is_Ad ~ Height + Width + Aspect_ratio, training,
     family = binomial()
 ) # adding aspect ratio
-summary(model2)
-anova(model1, model2)
 
 model3 <- glm(Is_Ad ~ Height + Width + Aspect_ratio + Local, training,
     family = binomial()
 ) # adding local
+
+
+summary(model1)
+summary(model2)
 summary(model3)
+
+
+# Compare models using anova
+# Does Aspect_ratio improve the Height+Width model?
+anova(model1, model2)
+
+# Does Local improve the Height+Width+Aspect_ratio model?
 anova(model2, model3)
-
-model4 <- glm(Is_Ad ~ Local * (Height + Width + Aspect_ratio),
-    training,
-    family = binomial()
-) # testing interaction
-summary(model4)
-anova(model2, model4)
-
 
 # Choose final model as model2
 model_final <- model2
+
+# Use vif to check for multicollinearity:
+library(car)
+vif(model_final)
 
 # Assess importance of each predictor by 
 # dropping one at a time and measure how worse the model is
 drop1(model_final, test = "Chisq")
 
-# Confidence intervals of the coefficients of predictors
+# Confidence intervals for the coefficients of predictors
 confint(model_final)
 
 # Get odd ratios of each predictor
-# OR > 1: increases odds of outcome
-# OR < 1: decreases odds of outcome
+# OR > 1: increases odds of positive outcome
+# OR < 1: decreases odds of positive outcome
 exp(coef(model_final))
+
 
 # Make a prediction object
 probs <- predict(model_final, newdata = testing, type = "response")
